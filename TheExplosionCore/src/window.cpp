aba8611ea33c7a2066d3852cd211e3c6b26f4f25
SlipSlup <hameleon1900@gmail.com>
2025-05-23 16:29:19 +0300

    commit

--- TheExplosionCore/src/window.cpp
+++ TheExplosionCore/src/window.cpp
@@ -12,6 +12,7 @@
 #include "OpenGL/index_buffer.hpp"
 #include <glm/mat3x3.hpp>
 #include <glm/trigonometric.hpp>
+#include "camera.hpp"
 
 namespace TheExplosion {
 
@@ -26,11 +27,16 @@ namespace TheExplosion {
 
     };
 
-    GLuint indices[] = { 0, 1, 2, 3, 2, 1 };
+    GLuint square_indices[] = { 0, 1, 2, 3, 2, 1 };
 
-    float scale[3] = { 1.0f, 1.0f, 1.0f };
-    float rotation = 0.0f;
-    float translation[3] = { 0.0f, 0.0f, 0.0f };
+    float square_scale[3] = { 1.0f, 1.0f, 1.0f };
+    float square_rotation = 0.0f;
+    float square_translation[3] = { 0.0f, 0.0f, 0.0f };
+
+    float camera_position[3] = { 0.0f, 0.0f, 1.0f };
+    float camera_rotation[3] = { 0.0f, 0.0f, 0.0f };
+    bool perspective_camera = true;
+    Camera camera;
 
     const char* vertex_shader = R"(
         
@@ -39,12 +45,13 @@ namespace TheExplosion {
         layout(location = 0) in vec3 vertex_position;
         layout(location = 1) in vec3 vertex_color;
         uniform mat4 model_matrix;
+        uniform mat4 view_projection_matrix;
         out vec3 color;
         
         void main() {
         
             color = vertex_color;
-            gl_Position = model_matrix * vec4(vertex_position, 1.0);
+            gl_Position = view_projection_matrix * model_matrix * vec4(vertex_position, 1.0);
         
         }
         
@@ -123,7 +130,7 @@ namespace TheExplosion {
         p_shader_program = std::make_unique<ShaderProgram>(vertex_shader, fragment_shader);
         p_vao = std::make_unique<VertexArray>();
         p_positions_colors_vbo = std::make_unique<VertexBuffer>(square_data, sizeof(square_data), buffer_layout_2vec3);
-        p_index_buffer = std::make_unique<IndexBuffer>(indices, sizeof(indices) / sizeof(GLuint));
+        p_index_buffer = std::make_unique<IndexBuffer>(square_indices, sizeof(square_indices) / sizeof(GLuint));
         p_vao->add_vertex_buffer(*p_positions_colors_vbo);
         p_vao->set_index_buffer(*p_index_buffer);
 
@@ -140,44 +147,59 @@ namespace TheExplosion {
 
 	void Window::on_update() {
         
-        glm::mat4 scale_matrix(
+        glm::mat4 square_scale_matrix(
 
-            scale[0], 0,        0,        0,
-            0,        scale[1], 0,        0,
-            0,        0,        scale[2], 0,
-            0,        0,        0,        1
-
-        );
-
-        float rotation_in_radians = glm::radians(rotation);
-
-        glm::mat4 rotation_matrix(
-
-             cos(rotation_in_radians), sin(rotation_in_radians), 0, 0,
-            -sin(rotation_in_radians), cos(rotation_in_radians), 0, 0,
-             0,                        0,                        1, 0,
-             0,                        0,                        0, 1
+            square_scale[0], 0,               0,               0,
+            0,               square_scale[1], 0,               0,
+            0,               0,               square_scale[2], 0,
+            0,               0,               0,               1
 
         );
 
-        glm::mat4 translation_matrix(
+        float square_rotation_in_radians = glm::radians(square_rotation);
 
-            1,              0,              0,              0,
-            0,              1,              0,              0,
-            0,              0,              1,              0,
-            translation[0], translation[1], translation[2], 1
+        glm::mat4 square_rotation_matrix(
+
+             cos(square_rotation_in_radians), sin(square_rotation_in_radians), 0, 0,
+            -sin(square_rotation_in_radians), cos(square_rotation_in_radians), 0, 0,
+             0,                               0,                               1, 0,
+             0,                               0,                               0, 1
 
         );
+
+        glm::mat4 square_translation_matrix(
+
+            1,                     0,                     0,                     0,
+            0,                     1,                     0,                     0,
+            0,                     0,                     1,                     0,
+            square_translation[0], square_translation[1], square_translation[2], 1
+
+        );
+
+        glm::mat4 square_model_matrix = square_translation_matrix * square_rotation_matrix * square_scale_matrix;
         
         glClearColor(m_background_color[0], m_background_color[1], m_background_color[2], 1);
         glClear(GL_COLOR_BUFFER_BIT);
+
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
+
         p_shader_program->bind();
-        glm::mat4 model_matrix = translation_matrix * rotation_matrix * scale_matrix;
-        p_shader_program->setMatrix4("model_matrix", model_matrix);
+        p_shader_program->setMatrix4("model_matrix", square_model_matrix);
+
+        camera.set_position_rotation(
+        
+            glm::vec3(camera_position[0], camera_position[1], camera_position[2]),
+            glm::vec3(camera_rotation[0], camera_rotation[1], camera_rotation[2])
+        
+        );
+
+        camera.set_projection_mode(perspective_camera ? Camera::ProjectionMode::Perspective : Camera::ProjectionMode::Orthographic);
+        p_shader_program->setMatrix4("view_projection_matrix", camera.get_projection_matrix() * camera.get_view_matrix());
+
         p_vao->bind();
+
         glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(p_vao->get_indices_count()), GL_UNSIGNED_INT, nullptr);
 
         if(ImGui::IsKeyPressed(ImGuiKey_Escape)) leave = !leave;
@@ -199,23 +221,21 @@ namespace TheExplosion {
         }
 
         ImGui::Begin("Menu");
-
         ImGui::Text("Background");
-
         ImGui::ColorEdit3("Background Color", m_background_color);
-
         ImGui::Text("Square");
-
-        ImGui::SliderFloat3("Square Scale", scale, 0.0f, 2.0f);
-
-        ImGui::SliderFloat("Square Rotation", &rotation, 0.0f, 360.0f);
-
-        ImGui::SliderFloat3("Square Translation", translation, -1.0f, 1.0f);
-        
+        ImGui::SliderFloat3("Square Scale", square_scale, 0.0f, 2.0f);
+        ImGui::SliderFloat("Square Rotation", &square_rotation, 180.0f, 180.0f);
+        ImGui::SliderFloat3("Square Translation", square_translation, -1.0f, 1.0f);
+        ImGui::Text("Camera");
+        ImGui::SliderFloat3("Camera Position", camera_position, -10.0f, 10.0f);
+        ImGui::SliderFloat3("Camera Rotation", camera_rotation, -180.0f, 180.0f);
+        ImGui::Checkbox("Perspective Camera", &perspective_camera);
         ImGui::End();
 
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         glfwSwapBuffers(m_pWindow);
         glfwPollEvents();
 
